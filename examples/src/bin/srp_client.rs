86f851d51250eb306cecf3e16d6f5f676386382d
Alexandra Clifford <a13xandra.cliff0rd@gmail.com>
2024-09-22 15:21:17 -0400

    Extend srp client example to unregister service once UDP packet is received

--- examples/src/bin/srp_client.rs
+++ examples/src/bin/srp_client.rs
@@ -20,10 +20,16 @@ use esp_openthread::{
 use esp_println::println;
 use static_cell::StaticCell;
 
-// Hostname must be null terminated string
-const HOSTNAME: &str = "ot-esp32-1\0";
-const SERVICENAME: &str = "ot-service1";
-const INSTANCENAME: &str = "_otpps._tcp";
+// May need to modify the hostname and service name values if a previous node
+// was up & successfully registers, but due to error/power event not able to
+// unregister the service. Host names and service names must be unique and are
+// accepted by SRP server on first come first serve basis.
+static HOSTNAME: Mutex<RefCell<&str>> = Mutex::new(RefCell::new("ot-esp32\0"));
+static SERVICENAME: Mutex<RefCell<&str>> = Mutex::new(RefCell::new("ot-service"));
+static INSTANCENAME: Mutex<RefCell<&str>> = Mutex::new(RefCell::new("_otpps._tcp"));
+static DNSTXT: Mutex<RefCell<&'static str>> = Mutex::new(RefCell::new(""));
+static SUBTYPES: Mutex<RefCell<&'static [&'static str]>> = Mutex::new(RefCell::new(&[]));
+
 const BOUND_PORT: u16 = 1212;
 
 #[entry]
@@ -68,7 +74,7 @@ fn main() -> ! {
         network_name: Some("OpenThread-58d1".try_into().unwrap()),
         extended_pan_id: Some([0x3a, 0x90, 0xe3, 0xa3, 0x19, 0xa9, 0x04, 0x94]),
         pan_id: Some(0x58d1),
-        channel: Some(25),
+        channel: Some(11),
         channel_mask: Some(0x07fff800),
         ..OperationalDataset::default()
     };
@@ -105,56 +111,92 @@ fn main() -> ! {
         });
 
         if register {
-            // Must be a NULL terminated string with static lifetime
-            if let Err(e) = openthread.setup_srp_client_set_hostname(HOSTNAME) {
-                log::error!("Error enabling srp client {e:?}");
-            }
+            critical_section::with(|cs| {
+                if let Err(e) = openthread.setup_srp_client_set_hostname(*HOSTNAME.borrow_ref(cs)) {
+                    log::error!("Error enabling srp client {e:?}");
+                }
+            });
 
             if let Err(e) = openthread.setup_srp_client_host_addr_autoconfig() {
                 log::error!("Error enabling srp client {e:?}");
             }
 
-            if let Err(e) = openthread.register_service_with_srp_client(
-                SERVICENAME,
-                INSTANCENAME,
-                &[],
-                "",
-                12345,
-                None,
-                None,
-                None,
-                None,
-            ) {
-                log::error!("Error registering service {e:?}");
-            }
+            critical_section::with(|cs| {
+                if let Err(e) = openthread.register_service_with_srp_client(
+                    *SERVICENAME.borrow_ref(cs),
+                    *INSTANCENAME.borrow_ref(cs),
+                    *SUBTYPES.borrow_ref(cs),
+                    *DNSTXT.borrow_ref(cs),
+                    12345,
+                    None,
+                    None,
+                    None,
+                    None,
+                ) {
+                    log::error!("Error registering service {e:?}");
+                } else {
+                    println!(
+                        "Registered SRP service; hostname {:?}, {:?}, {:?}",
+                        *HOSTNAME.borrow_ref(cs),
+                        *SERVICENAME.borrow_ref(cs),
+                        *INSTANCENAME.borrow_ref(cs)
+                    );
+                }
+            });
 
-            println!("Registered SRP service; hostname {HOSTNAME:}, {SERVICENAME:}, {INSTANCENAME:}");
             break;
         }
     }
 
-    let mut socket = openthread.get_udp_socket::<512>().unwrap();
-    let mut socket = pin!(socket);
-    socket.bind(BOUND_PORT).unwrap();
+    // restrict scope of socket (so we can mutably borrow openthread after we break out of loop)
+    {
+        let mut socket = openthread.get_udp_socket::<512>().unwrap();
+        let mut socket = pin!(socket);
+        socket.bind(BOUND_PORT).unwrap();
 
-    let mut buffer = [0u8; 512];
+        let mut buffer = [0u8; 512];
 
+        loop {
+            openthread.process();
+            openthread.run_tasklets();
+            let (len, from, port) = socket.receive(&mut buffer).unwrap();
+
+            // When the program receives a UDP packet, it will unregister SRP services
+            if len > 0 {
+                println!(
+                    "received {:02x?} from {:?} port {}",
+                    &buffer[..len],
+                    from,
+                    port
+                );
+                socket.send(from, BOUND_PORT, b"Hello").unwrap();
+                println!("Sent response message, now unregistering SRP service!");
+                break;
+            }
+
+            critical_section::with(|cs| {
+                let mut c = changed.borrow_ref_mut(cs);
+                if c.0 {
+                    let addrs: heapless::Vec<NetworkInterfaceUnicastAddress, 5> =
+                        openthread.ipv6_get_unicast_addresses();
+
+                    print_all_addresses(addrs);
+                    c.0 = false;
+                }
+            });
+        }
+    }
+
+    if let Err(e) = openthread.srp_unregister_all_services(true, true) {
+        log::error!("Failure to unregister all services {e:?}");
+    }
+
+    println!("SRP services unregistered, no longer receiving UDP packets");
+
+    let mut break_loop = false;
     loop {
         openthread.process();
         openthread.run_tasklets();
-        let (len, from, port) = socket.receive(&mut buffer).unwrap();
-        if len > 0 {
-            println!(
-                "received {:02x?} from {:?} port {}",
-                &buffer[..len],
-                from,
-                port
-            );
-
-            socket.send(from, BOUND_PORT, b"Hello").unwrap();
-            println!("Sent message");
-        }
-
         critical_section::with(|cs| {
             let mut c = changed.borrow_ref_mut(cs);
             if c.0 {
@@ -162,10 +204,22 @@ fn main() -> ! {
                     openthread.ipv6_get_unicast_addresses();
 
                 print_all_addresses(addrs);
+
+                if c.1.contains(ChangedFlags::ThreadRlocRemoved) {
+                    println!("Dropped from Thread network, resetting!");
+                    break_loop = true;
+                }
                 c.0 = false;
             }
         });
+        if break_loop {
+            break;
+        }
     }
+
+    esp_hal::reset::software_reset_cpu();
+    // This wont execute
+    loop {}
 }
 
 fn print_all_addresses(addrs: heapless::Vec<NetworkInterfaceUnicastAddress, 5>) {
